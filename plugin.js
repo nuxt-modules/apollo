e61d1d3ade9019fc3d84d97a9c0e5301fb000733
Umut Akkaya <Xicy@users.noreply.github.com>
2017-09-06 20:24:20 +0300

    Update plugin.js
    remove useless code blocks

--- plugin.js
+++ plugin.js
@@ -1,52 +1,40 @@
 import Vue from 'vue'
 import 'isomorphic-fetch'
 import VueApollo from 'vue-apollo'
-import { ApolloClient, createNetworkInterface } from 'apollo-client'
+import {ApolloClient} from 'apollo-client'
 
 Vue.use(VueApollo)
 
 export default (ctx) => {
+    const {isServer, app, beforeNuxtRender, isDev} = ctx
 
-  const providerOptions = {
-    clients: {}
-  }
-
-  const { isClient, isServer, app, route, beforeNuxtRender, store } = ctx
-
-  <% Object.keys(options.networkInterfaces).forEach((key) => { %>
-    let networkInterface = require('<%= options.networkInterfaces[key] %>')
-    networkInterface = networkInterface.default(ctx) || networkInterface(ctx)
-    const <%= key %>Client = new ApolloClient({
-      networkInterface,
-      ...(isServer ? {
-        ssrMode: true
-      } : {
-        initialState: window.__NUXT__.apollo.<%= key === 'default' ? 'defaultClient' : key %>,
-        ssrForceFetchDelay: 100
-      })
+    const providerOptions = {
+        clients: {}
+    }
+    /* <% Object.keys(options.networkInterfaces).forEach((key) => { if (key === 'default') { %> */
+    providerOptions[`<%= key %>Client`] = /* <% } else { %> */ providerOptions.clients[`<%= key %>`] = /* <% } %> */ new ApolloClient({
+        networkInterface: require('<%= options.networkInterfaces[key] %>').default(ctx) || require('<%= options.networkInterfaces[key] %>')(ctx),
+        ...(isServer ? {
+            ssrMode: true
+        } : {
+            initialState: window.__NUXT__.apollo[`<%= key === 'default' ? 'defaultClient' : key %>`],
+            ssrForceFetchDelay: 100,
+            connectToDevTools: isDev
+        })
     })
-    <% if (key === 'default') { %>
-      providerOptions.<%= key %>Client = <%= key %>Client
-    <% } else { %>
-      providerOptions.clients.<%= key %> = <%= key %>Client
-    <% } %>
+    /* <% }) %> */
 
-  <% }) %>
+    app.apolloProvider = new VueApollo(providerOptions)
 
-
-  app.apolloProvider = new VueApollo(providerOptions)
-
-  if (isServer) {
-    beforeNuxtRender(async ({ Components, nuxtState }) => {
-      Components.forEach((Component) => {
-        // Fix https://github.com/nuxt-community/apollo-module/issues/19
-        if (Component.options && Component.options.apollo && Component.options.apollo.$init) {
-          delete Component.options.apollo.$init
-        }
-      })
-      await app.apolloProvider.prefetchAll(ctx, Components)
-      nuxtState.apollo = app.apolloProvider.getStates()
-    })
-  }
-
+    if (isServer) {
+        beforeNuxtRender(async ({Components, nuxtState}) => {
+            Components.forEach((Component) => {
+                if (Component.options && Component.options.apollo && Component.options.apollo.$init) {
+                    delete Component.options.apollo.$init
+                }
+            })
+            await app.apolloProvider.prefetchAll(ctx, Components)
+            nuxtState.apollo = app.apolloProvider.getStates()
+        })
+    }
 }
