2ba1c5efa9dbb002d99c4a6d17297c876e0109d0
Dominic Garms <djgarms@gmail.com>
2018-06-08 09:11:10 +0800

    add cookie to vendor and fixed test

--- test/fixture/middleware/isAuth.js
+++ test/fixture/middleware/isAuth.js
@@ -1,6 +1,6 @@
 export default function ({app, error}) {
-    const hasToken = !!app.$apolloHelpers.getToken()
-    if (!hasToken) {
-        error({errorCode:503, message:'You are not allowed to see this'})
-    }
+  const hasToken = !!app.$apolloHelpers.getToken()
+  if (!hasToken) {
+    error({errorCode: 503, message: 'You are not allowed to see this'})
+  }
 }
--- test/module.test.js
+++ test/module.test.js
@@ -21,6 +21,6 @@ describe('basic', () => {
 
   test('render', async () => {
     let html = await get('/')
-    expect(html).toContain('Works!')
+    expect(html).toContain('This is the landing page')
   })
 })
