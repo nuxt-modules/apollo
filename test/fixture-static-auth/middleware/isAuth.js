c08bcb331db4b776a7cbec32ee0a3f68b9068abf
Negezor <negezor@gmail.com>
2018-11-18 18:45:39 +1100

    fix(eslint): Fix eslint errors

--- test/fixture-static-auth/middleware/isAuth.js
+++ test/fixture-static-auth/middleware/isAuth.js
@@ -1,6 +1,6 @@
-export default function ({app, error}) {
+export default function ({ app, error }) {
   const hasToken = !!app.$apolloHelpers.getToken()
   if (!hasToken) {
-    error({errorCode: 503, message: 'You are not allowed to see this'})
+    error({ errorCode: 503, message: 'You are not allowed to see this' })
   }
 }
--- test/fixture/nuxt.config.js
+++ test/fixture/nuxt.config.js
@@ -1,4 +1,4 @@
-const {resolve} = require('path')
+const { resolve } = require('path')
 require('dotenv').config()
 
 module.exports = {
