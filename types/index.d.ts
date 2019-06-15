d6e9db8ac9a8c57c2eda078aede914191c85a962
orblazer <orblazer.minecraft@gmail.com>
2019-06-16 00:35:01 +0200

    feat: add type definition for nuxt configuration

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,2 @@
 import './vue'
+import './nuxt'
---
+++ types/nuxt.d.ts
@@ -0,0 +1,28 @@
+/**
+ * Extends interfaces in Nuxt
+ */
+
+import NuxtConfiguration from '@nuxt/config'
+import { VueApolloOptions } from 'vue-apollo/types/options'
+import { ApolloClientClientConfig } from 'vue-cli-plugin-apollo/graphql-client'
+
+declare module '@nuxt/config/types/index' {
+  interface ApolloClientConfig extends ApolloClientClientConfig<any> {
+    httpEndpoint: string;
+  }
+
+  export default interface NuxtConfiguration {
+    apollo: {
+      tokenName?: string
+      tokenExpires?: number
+      includeNodeModules?: boolean
+      authenticationType?: string
+      errorHandler?: string
+      defaultOptions?: VueApolloOptions<any>
+      clientConfigs: {
+        default: ApolloClientConfig | string
+        [key: string]: ApolloClientConfig | string
+      }
+    }
+  }
+}
