e7c776e9aa255859be4cd4c1806a57773f7a76fa
orblazer <orblazer.minecraft@gmail.com>
2019-06-16 00:34:26 +0200

    fix: allow any cache on client

--- types/vue.d.ts
+++ types/vue.d.ts
@@ -8,8 +8,8 @@ import { ApolloClient } from 'apollo-client'
 declare module 'vue/types/vue' {
   interface Vue {
     $apolloHelpers: {
-      onLogin (token: string, apolloClient?: ApolloClient<{}>, tokenExpires?: number): Promise<void>
-      onLogout (apolloClient?: ApolloClient<{}>): Promise<void>
+      onLogin (token: string, apolloClient?: ApolloClient<any>, tokenExpires?: number): Promise<void>
+      onLogout (apolloClient?: ApolloClient<any>): Promise<void>
       getToken (tokenName?: string): string
     }
   }
