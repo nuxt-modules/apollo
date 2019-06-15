5b1a20464f08e802ef46cbb2c43dfbde94f3de27
orblazer <orblazer.minecraft@gmail.com>
2019-06-16 01:29:25 +0200

    fix(types): add $apolloHelpers on context

--- types/nuxt.d.ts
+++ types/nuxt.d.ts
@@ -5,10 +5,12 @@
 import NuxtConfiguration from '@nuxt/config'
 import { VueApolloOptions } from 'vue-apollo/types/options'
 import { ApolloClientClientConfig } from 'vue-cli-plugin-apollo/graphql-client'
+import Vue, { ComponentOptions } from 'vue'
+import { ApolloHelpers } from '.'
 
 declare module '@nuxt/config/types/index' {
   interface ApolloClientConfig extends ApolloClientClientConfig<any> {
-    httpEndpoint: string;
+    httpEndpoint: string
   }
 
   export default interface NuxtConfiguration {
@@ -24,5 +26,11 @@ declare module '@nuxt/config/types/index' {
         [key: string]: ApolloClientConfig | string
       }
     }
+  }
+}
+
+declare module '@nuxt/vue-app/types/index' {
+  interface NuxtAppOptions extends ComponentOptions<Vue> {
+    $apolloHelpers: ApolloHelpers
   }
 }
--- types/vue.d.ts
+++ types/vue.d.ts
@@ -4,13 +4,10 @@
 
 import Vue from 'vue'
 import { ApolloClient } from 'apollo-client'
+import { ApolloHelpers } from '.'
 
 declare module 'vue/types/vue' {
   interface Vue {
-    $apolloHelpers: {
-      onLogin (token: string, apolloClient?: ApolloClient<any>, tokenExpires?: number): Promise<void>
-      onLogout (apolloClient?: ApolloClient<any>): Promise<void>
-      getToken (tokenName?: string): string
-    }
+    $apolloHelpers: ApolloHelpers
   }
 }
