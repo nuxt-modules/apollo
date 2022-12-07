1d71e6361541fa422e6c9aab073c09b58b7c1adf
Yaël Guilloux <yael.guilloux@gmail.com>
2022-12-07 03:33:23 +0100

    feat(docs): upgrade to latest Docus (#457)

---
+++ docs/app.config.ts
@@ -0,0 +1,16 @@
+export default defineAppConfig({
+  docus: {
+    aside: {
+      level: 0,
+    },
+    header: {
+      title: "Nuxt Apollo",
+      logo: true,
+    },
+    image: "/cover.jpg",
+    socials: {
+      github: "nuxt-modules/apollo-module",
+      twitter: "@nuxt_js",
+    },
+  }
+});
--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,33 +1,7 @@
-import { defineNuxtConfig } from 'nuxt'
-
 export default defineNuxtConfig({
   extends: ['@nuxt-themes/docus'],
 
   colorMode: {
     preference: 'dark'
   },
-
-  tailwindcss: {
-    config: {
-      important: true,
-      theme: {
-        extend: {
-          colors: {
-            primary: {
-              100: '#F3E8FF',
-              200: '#E9D5FF',
-              300: '#D8B4FE',
-              400: '#C084FC',
-              500: '#A855F7',
-              600: '#9333EA',
-              700: '#7E22CE',
-              800: '#6B21A8',
-              900: '#581C87'
-            }
-          }
-        }
-      }
-    }
-  }
-
 })
